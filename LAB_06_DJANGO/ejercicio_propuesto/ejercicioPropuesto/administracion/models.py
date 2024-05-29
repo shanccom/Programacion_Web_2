2c4253571c742a17e782ed833582b3759fad54bc
Sergio Danilo Hancco Mullisaca <shanccom@unsa.edu.pe>
2024-05-28 23:20:50 -0500

    Creacion de los modelos en la app: administracion

--- LAB_06_DJANGO/ejercicio_propuesto/ejercicioPropuesto/administracion/models.py
+++ LAB_06_DJANGO/ejercicio_propuesto/ejercicioPropuesto/administracion/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# Creacion de modelos propios
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length = 50)
+    apellido = models.CharField(max_length = 50)
+    cui = models.CharField(max_length = 8, unique = True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length = 20)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+class NotasAlumno(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete = models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete = models.CASCADE)
+    nota = models.DecimalField(max_digits = 5, decimal_places = 2)
+
+    def __str__(self):
+        return f"{self.alumno} - {self.curso}: {self.nota}"
