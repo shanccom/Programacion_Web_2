2927a3d0dfa328b14c8ec8b45183ea94a2977e76
Sergio Danilo Hancco Mullisaca <shanccom@unsa.edu.pe>
2024-06-14 18:57:34 -0500

    LAB TERMINADO - INFORMES

--- LAB_08_DJANGO3/Ejecucion_de_Videos/6.  VIDEO/emailI/emailI/settings.py
+++ LAB_08_DJANGO3/Ejecucion_de_Videos/6.  VIDEO/emailI/emailI/settings.py
@@ -126,14 +126,6 @@ STATIC_URL = 'static/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sergiodanilohancco@gmail.com'
+EMAIL_HOST_USER = 'correo@gmail.com'
 EMAIL_USE_TLS = True    
 
-
-
-
-
-
-
-
-EMAIL_HOST_PASSWORD = 'lmyi mbqf zoyj lfqc'
