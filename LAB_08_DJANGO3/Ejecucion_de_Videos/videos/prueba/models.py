23d38ce466c7d8415b147927fc69156380070998
Sergio Danilo Hancco Mullisaca <shanccom@unsa.edu.pe>
2024-06-14 09:05:04 -0500

    Modificacion de models

---
+++ LAB_08_DJANGO3/Ejecucion_de_Videos/videos/prueba/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Languaje(models.Model):
+    name = models.CharField(max_length = 10)
+
+    def __str__(self):
+        return self.name
+        
+class FrameWork(models.Model):
+    name = models.CharField(max_length = 10)
+    languaje = models.ForeignKey(Languaje, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
