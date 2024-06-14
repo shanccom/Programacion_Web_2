348d1f1dc364563c27f364b53b125bc8cfbbd194
Sergio Danilo Hancco Mullisaca <shanccom@unsa.edu.pe>
2024-06-14 09:29:09 -0500

    Ejecucion del tercer video

--- LAB_08_DJANGO3/Ejecucion_de_Videos/videos/prueba/models.py
+++ LAB_08_DJANGO3/Ejecucion_de_Videos/videos/prueba/models.py
@@ -14,3 +14,16 @@ class FrameWork(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Movie(models.Model):
+    name = models.CharField(max_length = 10)
+
+    def __str__(self):
+        return self.name
+    
+class Character(models.Model):
+    name = models.CharField(max_length = 10)
+    movies = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return self.name
